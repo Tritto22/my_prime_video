2c97b600d032eac37e723c53a97b065927587d88
Simone <tritto.simone@gmail.com>
2023-06-27 11:35:29 +0200

    init_searchbar

---
+++ src/components/macro/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <SearchBar/>
+</template>
+
+<script>
+import SearchBar from '../commons/SearchBar.vue'
+export default {
+    name: 'Header',
+    components: {
+        SearchBar
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

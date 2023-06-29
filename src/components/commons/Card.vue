ac00a0540155230a6f50f4392f22c0d806ed80b1
Simone <tritto.simone@gmail.com>
2023-06-29 15:10:02 +0200

    implement_card_with_hover

--- src/components/commons/Card.vue
+++ src/components/commons/Card.vue
@@ -1,18 +1,24 @@
 <template>
-    <div class="card">
-        <div v-if="context == 'movie'" class="movie">            
-            <h3 class="titol">{{ array.title }}</h3>
-            <h4 class="original-titol">{{ array.original_title }}</h4>
-            <img class="lang" v-if="flag.includes(array.original_language)" :src="require(`../../assets/img/${array.original_language}.png`)" :alt="array.original_language">
-            <img class="lang" v-else src="../../assets/img/world.png" :alt="array.original_language">
-            <h4 class="vote">{{ array.vote_average }}</h4>
-        </div>
-        <div v-else-if="array == 'tv'" class="tv">
-            <h3 class="titol">{{ array.name }}</h3>
-            <h4 class="original-titol">{{ array.original_name }}</h4>
-            <img class="lang" v-if="flag.includes(array.original_language)" :src="require(`../../assets/img/${array.original_language}.png`)" :alt="array.original_language">
-            <img class="lang" v-else src="../../assets/img/world.png" :alt="array.original_language">
-            <h4 class="vote">{{ array.vote_average }}</h4>
+    <div class="card" :style="{ 'background-image': cardBackground }">
+        <div class="card-info">
+            <div v-if="context == 'movie'" class="movie">
+                <div class="description">
+                    <h3 class="titol">{{ array.title }}</h3>
+                    <h4 class="original-titol">{{ array.original_title }}</h4>
+                    <img class="lang" v-if="flag.includes(array.original_language)" :src="require(`../../assets/img/${array.original_language}.png`)" :alt="array.original_language">
+                    <img class="lang" v-else src="../../assets/img/world.png" :alt="array.original_language">
+                    <h4 class="vote">{{ array.vote_average }}</h4>
+                </div>            
+            </div>
+            <div v-else-if="array == 'tv'" class="tv">
+                <div class="description">
+                    <h3 class="titol">{{ array.name }}</h3>
+                    <h4 class="original-titol">{{ array.original_name }}</h4>
+                    <img class="lang" v-if="flag.includes(array.original_language)" :src="require(`../../assets/img/${array.original_language}.png`)" :alt="array.original_language">
+                    <img class="lang" v-else src="../../assets/img/world.png" :alt="array.original_language">
+                    <h4 class="vote">{{ array.vote_average }}</h4>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -29,18 +35,32 @@ export default {
     data() {
         return {
             dataShared,
-            flag: ['de', 'en', 'es', 'fr', 'it', 'nl', 'us']
+            flag: ['de', 'en', 'es', 'fr', 'it', 'nl', 'us'],
+            cardBackground: `url('https://image.tmdb.org/t/p/w500${this.array.poster_path}')`
         }
     },
     methods: {
-        // log(){
-        //     console.log(this.context);
-        // }
+        log(){
+            console.log(this.pathBg);
+        }
     }
 }
 </script>
 
 <style lang="scss" scoped>
+.card{
+    object-fit: contain;
+    background-repeat: no-repeat;
+    background-size: contain;
+    height: 500px;
+    .card-info{
+    display: none;
+    }
+    &:hover .card-info{
+        display: block;
+    }
+}
+
 .lang {
     width: 50px;
 }
